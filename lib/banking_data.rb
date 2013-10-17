854b67a6df24e89ac1d465aff183a2bace4037ae
Frank C. Eckert <frank.eckert@boost-project.com>
2013-10-17 14:42:27 +0200

    more requires

--- lib/banking_data.rb
+++ lib/banking_data.rb
@@ -1,6 +1,9 @@
 require 'active_model'
 require 'banking_data/version'
+require 'banking_data/bank'
 require 'banking_data/german_bank'
+require 'banking_data/austrian_bank'
+require 'banking_data/swiss_bank'
 
 module BankingData
 
